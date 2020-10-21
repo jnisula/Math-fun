f1c64770a306dfdc76dfb262960a76c20766c4ee
jnisula <janne.nisula@gmail.com>
2020-10-21 17:25:27 +0300

    more colors and window height

--- mandel.py
+++ mandel.py
@@ -3,7 +3,7 @@ from graphics import *
 from operator import *
 
 #graph window initialisation
-width, height = 1800, 1000
+width, height = 1800, 1200
     
 win = GraphWin('Graphics', width, height)
 win.setCoords(-width/2, -height/2, width/2, height/2)    
@@ -17,7 +17,7 @@ xAxisLine = Line(Point(0,-height/2),Point(0,height/2))
 xAxisLine.setOutline('lightgray')
 xAxisLine.draw(win)
 
-pc = height / 2 # plotting multiplier
+pc = 450 # plotting multiplier
 increment = 2 / pc
 yLimit = 1.3
 xUpperLimit = 0.6
@@ -25,8 +25,13 @@ print (pc)
 print (increment)
 nMax = 650
 
-def drawPoint(point, color1, color2):    
-    point.setOutline(color_rgb(0,color1,color2))
+#red point to origin for verifying scaling
+point = Point(0,0)
+point.setOutline("red")
+point.draw(win)
+
+def drawPoint(point, red, green, blue):    
+    point.setOutline(color_rgb(red, green, blue))
     point.draw(win)
     
 def main():
@@ -34,11 +39,11 @@ def main():
     tx = -2.0
 
     while (tx < xUpperLimit):
-        ty = -yLimit
+        ty = 0#-yLimit
         while (ty < yLimit):
             # run the z1 = z^2 + c for each point n times
-            zx = 0
-            zy = 0
+            zx = 0.0
+            zy = 0.0
             
             n = 0
             while (sqrt(zx ** 2 + zy ** 2) < 2.0 and n < nMax):
@@ -50,16 +55,35 @@ def main():
                 n = n + 1
 
             if (n >= nMax):
-                color1 = 0
-                color2 = 0 
+                red = 0
+                green = 0
+                blue = 0
+            elif (n <= 12):
+                red = 0
+                green = n * 20
+                blue = 255
+            elif (n <= 24):
+                red = (n-12) * 20
+                green = 255
+                blue = 255
+            elif (n <= 36):
+                red = (n-24) * 20
+                green = 0
+                blue = 255
+            elif (n <= 48):
+                red = (n-36) * 20
+                green = 0
+                blue = 0
             else:
-                color1 = 0
-                color2 = 255-n
-                if (color2 < 0):
-                    color2 = 0
+                red = 255
+                green = 255
+                blue = 255-n
+                if (blue < 0):
+                    blue = 0
                     
             if (n > 3):
-                drawPoint(Point(tx*pc,ty*pc),color1, color2)
+                drawPoint(Point(tx*pc,ty*pc),red, green, blue)
+                drawPoint(Point(tx*pc,-ty*pc),red, green, blue)
                 
             #if (n >= 600):
                 #print(n)
