b68e4d65941264b7e1bd497e76a104cea15aa472
jnisula <janne.nisula@gmail.com>
2020-10-22 17:10:34 +0300

    Different printing: calculate values to 2 dimensional array first, then print it as whole

--- mandel.py
+++ mandel.py
@@ -1,83 +1,39 @@
 from math import *
 from graphics import *
 from operator import *
+from PIL import Image
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 #graph window initialisation
-width, height = 600, 400
+width, height = 800, 800
+
+nMax = 100
+
+pointMap = np.zeros((height, width, 3), dtype=np.uint8)
     
-win = GraphWin('Graphics', width, height)
-win.setCoords(-width/2, -height/2, width/2, height/2)    
-win.setBackground(color_rgb(0,0,255))
+def iterate(start, increment, ZCX, ZCY):
+    tx = start[0]
 
-#draw axii
-xAxisLine = Line(Point(-width/2,0),Point(width/2,0))
-xAxisLine.setOutline('lightgray')
-xAxisLine.draw(win)
-xAxisLine = Line(Point(0,-height/2),Point(0,height/2))
-xAxisLine.setOutline('lightgray')
-xAxisLine.draw(win)
+    for x in range(len(pointMap[0])):        
+        ty = start[1]
 
+        for y in range(len(pointMap)):                    
 
-#print (pc)
-#print (increment)
-nMax = 1650
-
-#red point to origin for verifying scaling
-point = Point(0,0)
-point.setOutline("red")
-point.draw(win)
-
-def drawPoint(point, red, green, blue):    
-    point.setOutline(color_rgb(red, green, blue))
-    point.draw(win)
-    
-def iterate(xStart, yStart, xLimit, yLimit, pc, increment, ZCX, ZCY):
-
-    tx = xStart
-
-    while (tx < xLimit):
-        #print (tx)
-        ty = yStart
-        while (ty < yLimit):
             # run the z1 = z^2 + c for each point n times
 
             z = 0+0j
-            z2 = 40.0+0j            
             n = 0
-            same1 = False
-            same2 = False
             while (abs(z) < 2.0 and n < nMax):
                 c = tx + ty*1j
-                
-                z1 = z ** 2 + c
-                
-                
-                if (same1 == False and abs(z - z1) < 0.001):
-                    same1 = True
-                if (same2 == False and abs(z1 - z2) < 0.001):
-                    same2 = True
-                    
-                z2 = z
-
-                z = z1
-                
+                z = z ** 2 + c                                
                 n = n + 1
 
             if (n >= nMax): #black
-                red = 180
-                green = 180
-                blue = 180
-                if (same1):
-                    red = 0
-                    green = 0
-                    blue = 0
-                elif (same2):
-                    red = 120
-                    green = 60
-                    blue = 40
+                red = 0
+                green = 0
+                blue = 0                
             elif (n <= 12): #blue max -> green+
                 red = 0
                 green = n * 20
@@ -107,24 +63,29 @@ def iterate(xStart, yStart, xLimit, yLimit, pc, increment, ZCX, ZCY):
                 if (green > 255):
                     green = 255
                     
-            if (n > 1):
-                drawPoint(Point((tx-ZCX)*pc,(ty-ZCY)*pc),red, green, blue)                
-                #drawPoint(Point(tx*pc,-ty*pc),red, green, blue)
+            if (n > 0):                
+                pointMap[y][x] = (red, green, blue)
+                
                 
             #if (n >= 600):
                 #print(n)
             ty = ty + increment
             #print(str(tx) + "|" + str(ty))
         tx = tx + increment
+    
+    plt.imshow(pointMap, interpolation="nearest")
+    plt.show()
+    
+    #img = Image.fromarray(pointMap)
+    #img.save("mandelTest.png")
         
 def main():
-    pc = width / 4 # plotting multiplier
-    increment = 2 / pc
-    yLimit = 1.5
-    xLimit = 2
-    xStart = -2.0
-    yStart = -1.5
-    iterate(xStart, yStart, xLimit, yLimit, pc, increment, 0, 0) #zoomed out
+    size = 4
+    increment = size / width
+        
+    start = (-2.0,-2.0)
+    iterate(start, increment, 0, 0) #zoomed out
+    
     ZCX = -1.4035 #Zoom center x
     ZCY = 0.0294 #Zoom center y
     ZW = 0.001 #Zoom wdth
